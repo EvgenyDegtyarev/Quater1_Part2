1cc5fc059ad22c888b54a445f16da1236c5cbb60
EvgenyDegtyarev <e.degtyarev@icloud.com>
2021-04-11 18:47:10 +0300

    les3

---
+++ VK GeekBrains/VK GeekBrains/Views/Reusable/FriendPhotoViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FriendPhotoViewCell.swift
+//  VK GeekBrains
+//
+//  Created by Евгений Дегтярев on 11.04.2021.
+//
+
+import UIKit
+
+class FriendPhotoViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var friendsPhotoImage: UIImageView!
+}
